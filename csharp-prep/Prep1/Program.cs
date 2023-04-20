24286da17d092faf76984145b3ef2ac20594df73
romerodav <romerodav@hotmail.com>
2023-04-19 21:51:12 -0300

    Updating Prep1 Program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        string FirstName, LastName;
+
+        Console.Write("What is your first name? ");
+        FirstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}");
     }
 }
