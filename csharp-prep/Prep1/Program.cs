0acdd6c1afb98218f9e7cbdb159c5615fe847a36
romerodav <romerodav@hotmail.com>
2023-04-22 01:31:16 -0300

    Loop added

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,14 +6,26 @@ class Program
     {
         Console.WriteLine("Hello Prep1 World!");
 
-        string FirstName, LastName;
+        string FirstName, LastName, Value;
 
-        Console.Write("What is your first name? ");
-        FirstName = Console.ReadLine();
+        do
+        {
+            Console.Write("What is your first name? ");
+            FirstName = Console.ReadLine();
 
-        Console.Write("What is your last name? ");
-        LastName = Console.ReadLine();
+            Console.Write("What is your last name? ");
+            LastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}");
+            Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}");
+
+            Console.Write("Do you wish to use this program again? Y/N: ");
+            Value = Console.ReadLine();
+
+            Console.Clear();
+
+        }while (Value == "Y" | Value == "y");
+
+        Console.WriteLine("Thanks for using this program! Please come back soon");
+
     }
 }
