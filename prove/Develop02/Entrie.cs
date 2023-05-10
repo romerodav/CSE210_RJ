1e66c45eb0c8782f6e594f22e39b9b0205686b1a
romerodav <romerodav@hotmail.com>
2023-05-10 00:30:49 -0300

    Adding generic constructor to Entrie class

--- prove/Develop02/Entrie.cs
+++ prove/Develop02/Entrie.cs
@@ -2,6 +2,10 @@ public class Entrie
 {
     public string _quest, _ans, _dateTimeText;
 
+    public Entrie()
+    {
+    }
+
     public void ShowEntrie()
     {
         Console.WriteLine($"Date and Time: {_dateTimeText} - Question: {_quest}\nYour answer: {_ans}");
