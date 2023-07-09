78ac2f6f1f78a941757c78b7d8b2390195132865
romerodav <romerodav@hotmail.com>
2023-07-09 03:41:14 -0300

    ortographic mistake corrected

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -105,7 +105,7 @@ class Program
                         else
                         {
                             videos.AddRange(videoRepository);
-                            Console.WriteLine("Repostory Added");
+                            Console.WriteLine("Repository Added");
                         }
                     }
                 }
