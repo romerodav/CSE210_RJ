f1ab68280d3f90a771bb83ea623fd935e9a32d59
romerodav <romerodav@hotmail.com>
2023-07-13 02:09:36 -0300

    Order class finished

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,113 @@
+public class Order
+{
+    private Customer _customer;
+    private List<Product> _products;
+
+    public Order(string custName = "", string number = "", string street = "", string city = "", string stateOrProv = "", string country = "")
+    {
+        _customer = new Customer(custName, number, street, city, stateOrProv, country);
+        _products = new List<Product>();
+    }
+
+    public void Init(string custName, string number, string street, string city, string stateOrProv, string country)
+    {
+        _customer = new Customer(custName, number, street, city, stateOrProv, country);
+    }
+
+    public string GetCustName()
+    {
+        return _customer.GetName();
+    }
+
+    public void SetCustName(string custName)
+    {
+        _customer.SetName(custName);
+    }
+
+    public string GetAddressNumber()
+    {
+        return _customer.GetNumber();
+    }
+
+    public void SetAddressNumber(string addresNumber)
+    {
+        _customer.SetNumber(addresNumber);
+    }
+
+    public string GetStreet()
+    {
+        return _customer.GetStreet();
+    }
+
+    public void SetStreet(string street)
+    {
+        _customer.SetStreet(street);
+    }
+
+    public string GetCity()
+    {
+        return _customer.GetCity();
+    }
+
+    public void SetCity(string city)
+    {
+        _customer.SetCity(city);
+    }
+
+    public string GetStateOrProv()
+    {
+        return _customer.GetStateOrProv();
+    }
+
+    public void SetStateOrProv(string stateOrProv)
+    {
+        _customer.SetStateOrProv(stateOrProv);
+    }
+
+    public string GetCountry()
+    {
+        return _customer.GetCountry();
+    }
+
+    public void SetCountry(string country)
+    {
+        _customer.SetCountry(country);
+    }
+
+    public double GetShippingCost()
+    {
+        if(_customer.LiveInUSA() != 0) return 5.00;
+        else return 35.00;
+    }
+
+    public void AddProduct(string name, int id, float price, int quantity)
+    {
+        _products.Add(new Product(name, id, price, quantity));
+    }
+
+    public string ShippingLabel()
+    {
+        return $"{_customer.GetAddress()}\nShipping Cost $ {GetShippingCost().ToString("0.##")}";    
+    }
+
+    public string PackingLabel()
+    {
+        double subTotal = 0;
+        string packingLabel = "Product Name     Product Id    Price      Quantity     SubTotal\n";
+        foreach(Product product in _products)
+        {
+            packingLabel += product.List() + "\n";
+            subTotal += product.SubT();
+        }
+        packingLabel += $"SubTotal $ {subTotal.ToString("0.##")}";
+        return packingLabel;
+    }
+
+    public double TotalCost()
+    {
+        double totalCost = 0;
+        foreach(Product product in _products) totalCost += product.SubT();
+        totalCost += GetShippingCost();
+        return totalCost;
+    }
+}
---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,80 @@
+public class Product
+{
+    private string _name;
+    private int _id, _quantity;
+    private double _price;
+
+    public Product()
+    {
+        _name = "";
+        _id = 0;
+        _price = 0;
+        _quantity = 0;       
+    }
+
+    public Product(string name, int id, double price = 0, int quantity = 0)
+    {
+        _name = name;
+        _id = id;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public void Init(string name, int id, double price, int quantity)
+    {
+        _name = name;
+        _id = id;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public int GetId()
+    {
+        return _id;
+    }
+
+    public void SetId(int id)
+    {
+        _id = id;
+    }
+
+    public double GetPrice()
+    {
+        return _price;
+    }
+
+    public void SetPrice(double price)
+    {
+        _price = price;
+    }
+
+    public int GetQuantity()
+    {
+        return _quantity;
+    }
+
+    public void SetQuantity(int quantity)
+    {
+        _quantity = quantity;
+    }
+
+    public double SubT()
+    {
+        return _price * _quantity;
+    }
+
+    public string List()
+    {
+        return $"{_name,-16} {_id} {_price.ToString("0.##")} {_quantity} {(_price * _quantity).ToString("0.##")}";
+    }
+}
